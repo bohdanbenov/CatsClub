c62430a5710d0cf98fa8ed45742d3d837fc9b721
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-17 21:20:17 +0300

    Used dotenv gem for secure using api key.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -8,7 +8,7 @@ class CatsController < ApplicationController
     url = URI.parse('https://api.thecatapi.com/v1/images/search')
     request = Net::HTTP.get_response(url)
     request["Content-Type"] = 'application/json'
-    request["x-api-key"] = '8957a3ef-aa09-404f-a80a-b2fcc16c3267'
+    request["x-api-key"] = ENV['X_API_KEY']
     my_hash = JSON.parse(request.body)
     @url_image = my_hash[0]['url']
     @id_image = my_hash[0]['id']
