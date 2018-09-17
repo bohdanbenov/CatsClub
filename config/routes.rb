ec581503b24d0958a7a03a49e648c519ba5f257c
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-17 21:20:37 +0300

    Delete old comments from routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root("cats#index")
-  #resources :cats, only: [:index, :create] do
-  #  resources :favourites, only: [:index, :new]
-  #end
   resources :favourites, only: [:index, :new, :create]
   resources :images, only: [:show]
 end
