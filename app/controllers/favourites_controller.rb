724fbc0aa17e845e8e775f3a7dc93a001e9fdc8f
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-18 11:40:48 +0300

    Used request_to_api concern, create method which delete all empty hashes.

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -2,11 +2,23 @@ require 'net/http'
 require 'uri'
 
 class FavouritesController < ApplicationController
-  include FavouritePicturesId
-  include AddToFavourites
+  include RequestToApi
 
   def index
-    @all_favorites_links = get_favourite_ids
+    response_body = request_to_api('https://api.thecatapi.com/v1/favourites', 'get', 0, 0)
+
+    @all_favourite_links = []
+
+    response_body.each do |fav|
+      @all_favourite_links << {fav_image_id: fav['image_id']}
+    end
+
+    delete_empty_hash(@all_favourite_links)
+  end
+
+  def delete_empty_hash(arr_links)
+    arr_links.each{ |x| x.delete_if{|key, value| value.eql?nil}}
+    arr_links.delete_if { |hash| hash.empty? }
   end
 
   def new
@@ -14,7 +26,7 @@ class FavouritesController < ApplicationController
   end
 
   def create
-    @adding_result = add_to_favourites(params['q'], params['y'])
+    @adding_result = request_to_api('https://api.thecatapi.com/v1/favourites', 'post', params['q'], params['y'])
     render :new
   end
 end
