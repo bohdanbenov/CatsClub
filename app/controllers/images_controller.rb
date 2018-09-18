de66fe50fb02b9d30862844592d8e34b28572a71
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-18 12:25:56 +0300

    Made function for respond_to and used it in show action.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -6,11 +6,14 @@ class ImagesController < ApplicationController
   include RequestToApi
   def show
     response = request_to_api('https://api.thecatapi.com/v1/images/' + params['id'], 'get', 0, 0)
+    change_pictures_url(response)
+  end
 
+  def change_pictures_url(respond_from_api)
     respond_to do |format|
       format.json do
         render json: {
-            url: response['url']
+            url: respond_from_api['url']
         }
       end
     end
