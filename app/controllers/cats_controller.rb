f07cfec3c0f89ffab3a4d28405dec453cf18329c
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-18 11:39:20 +0300

    Used request_to_api concern and refactored code.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -4,14 +4,11 @@ require 'json'
 
 class CatsController < ApplicationController
   include GetCurrentTime
+  include RequestToApi
   def index
     @time = get_time
-    url = URI.parse('https://api.thecatapi.com/v1/images/search')
-    request = Net::HTTP.get_response(url)
-    request["Content-Type"] = 'application/json'
-    request["x-api-key"] = ENV['X_API_KEY']
-    my_hash = JSON.parse(request.body)
-    @url_image = my_hash[0]['url']
-    @id_image = my_hash[0]['id']
+    random_picture = request_to_api('https://api.thecatapi.com/v1/images/search', 'get', 0, 0)
+    @url_image = random_picture[0]['url']
+    @id_image = random_picture[0]['id']
   end
 end
